6466d92fc3ce5360074fb00de42544d53cf0974e
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-07-24 19:21:19 +0100

    add tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,47 @@
+'use strict'
 var test = require('tape')
-var lib = require('../')
+var Cache = require('../')
+var fs = require('fs')
+var path = require('path')
 
-test('first', function (t) {
+function createDir(name) {
+  var os = require('os')
+  var tmpDir = path.resolve(os.tmpdir(), 'cacheTest')
+  return tmpDir
+}
 
+function rmDir (dir) {
+  fs.readdirSync(dir).forEach(function (file) {
+    fs.unlinkSync(path.resolve(dir, file))
+  })
+  fs.rmdirSync(dir)
+}
+
+test('string key', function (t) {
+  t.plan(1)
+  var tmpDir = createDir()
+  var cache = new Cache({ cacheDir: tmpDir })
+  cache.write('one', 'test1')
+    .then(() => {
+      return cache.read('one')
+    })
+    .then(data => {
+      t.strictEqual(data, 'test1')
+      rmDir(tmpDir)
+    })
+})
+
+test('object key', function (t) {
+  t.plan(1)
+  var tmpDir = createDir()
+  var cache = new Cache({ cacheDir: tmpDir })
+  var objectKey = { one: true }
+  cache.write(objectKey, 'test1')
+    .then(() => {
+      return cache.read(objectKey)
+    })
+    .then(data => {
+      t.strictEqual(data, 'test1')
+      rmDir(tmpDir)
+    })
 })
